53367456eef5538cd8a22c883ec4efd915865a7f
zuesuep <kimsanz@gmail.com>
2024-09-05 20:04:38 +0800

    3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,28 +8,37 @@ let persons = [
     id: "1",
     name: "Arto Hellas",
     number: "040-123456",
-},
-{
+  },
+  {
     id: "2",
     name: "Ada Lovelace",
     number: "39-44-5323523",
-},
-{
+  },
+  {
     id: "3",
     name: "Dan Abramov",
     number: "12-43-234345",
-},
-{
+  },
+  {
     id: "4",
     name: "Mary Poppendieck",
     number: "39-23-6423122",
-},
+  },
 ];
 app.use(express.json());
 
 app.use(morgan("tiny"));
 
+morgan.token("postContent", (req) => {
+//   console.log(req);
+  return JSON.stringify(req.body);
+});
 
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time[5] ms :postContent"
+  )
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -57,7 +66,6 @@ app.get("/api/persons/:id", (req, res) => {
     res.status(404).end();
   }
 });
-
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
