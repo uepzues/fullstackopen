5772c912389c59fffee78c1132b7a32611c2588c
zuesuep <kimsanz@gmail.com>
2024-09-05 17:26:59 +0800

    3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -52,6 +52,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  person = persons.filter((p) => p.id === id);
+  console.log("DELETED", person);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
