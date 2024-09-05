8d4284b007034076225c1cc12df3e2927b1cfe32
zuesuep <kimsanz@gmail.com>
2024-09-05 17:17:51 +0800

    3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -40,6 +40,18 @@ app.get("/info", (req, res) => {
   );
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((a) => a.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    console.log("Object not found");
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
