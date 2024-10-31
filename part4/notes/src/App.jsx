da63a26222853a750e53fe15c1f1f4619569e6b9
zuesuep <kimsanz@gmail.com>
2024-10-31 21:09:40 +0800

    modified:   part4/notes/src/App.jsx

--- part4/notes/src/App.jsx
+++ part4/notes/src/App.jsx
@@ -1,74 +1,75 @@
-import { useState, useEffect } from "react";
-import Note from "./components/Note";
-import noteService from "./services/notes";
-import Notification from "./components/Notification";
-import Footer from "./components/Footer";
+import { useState, useEffect } from "react"
+import Note from "./components/Note"
+import noteService from "./services/notes"
+import Notification from "./components/Notification"
+import Footer from "./components/Footer"
 
 export default function App() {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
-      console.log(initialNotes);
-      setNotes(initialNotes.data);
-    });
-  }, []);
+      // console.log(initialNotes)
+      setNotes(initialNotes.data)
+    })
+  }, [])
 
   const toggleImportanceOf = (id) => {
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { content: note.content, important: !note.important }
 
     noteService
       .update(id, changedNote)
       .then((returnedNote) => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+        console.log(returnedNote)
+        setNotes(notes.map((n) => (n.id === id ? returnedNote : n)))
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error)
         setErrorMessage(
           `Note '${note.content}' was already removed from server`
-        );
+        )
         setTimeout(() => {
-          setErrorMessage(null);
-        }, 5000);
-      });
-  };
+          setErrorMessage(null)
+        }, 5000)
+      })
+  }
 
   const addNote = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-    };
+    }
 
     noteService.create(noteObject).then((returnedNote) => {
-      setNotes(notes.concat(returnedNote));
-      setNewNote("");
-    });
-  };
+      setNotes(notes.concat(returnedNote))
+      setNewNote("")
+    })
+  }
 
   const deleteNote = (id) => {
-    const note = notes.find((n) => n.id === id);
+    const note = notes.find((n) => n.id === id)
     noteService.delNote(note.id).then(() => {
-      setNotes(notes.filter((note) => note.id !== id));
-    });
-  };
+      setNotes(notes.filter((note) => note.id !== id))
+    })
+  }
 
   const handleNoteChange = (e) => {
-    console.log(e.target.value);
-    setNewNote(e.target.value);
-  };
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
 
   useEffect(() => {
     noteService.getAll().then((response) => {
-      setNotes(response.data);
-    });
-  }, []);
+      setNotes(response.data)
+    })
+  }, [])
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
@@ -96,5 +97,5 @@ export default function App() {
       </form>
       <Footer />
     </div>
-  );
+  )
 }
