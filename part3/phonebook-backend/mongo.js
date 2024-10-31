8341654ff5a6925f9b4dd646626dbc31def24347
zuesuep <kimsanz@gmail.com>
2024-10-31 14:54:33 +0800

    added validator to person schema

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -15,8 +15,22 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => v.length > 2,
+      message: (message) => `${message.value} is too short!`,
+    },
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => /\d{2,3}-\d{7,}/.test(v),
+      message: (message) => `${message.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
