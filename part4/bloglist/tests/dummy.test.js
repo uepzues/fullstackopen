acf3bee98be94001ecf377506c5ec414ec5ae4aa
zuesuep <kimsanz@gmail.com>
2024-11-03 20:42:31 +0800

    4.4 added test for total likes

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -10,3 +10,20 @@ test("dummy return one", () => {
 
   assert.strictEqual(result, 1)
 })
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      title: "The Joy of Japanese KitKats: Unique Flavors You Need to Try",
+      author: "Aki Tanaka",
+      url: "http://localhost:3004/post",
+      likes: 5,
+      id: "6726e6f460db2ccaa9e7a2e0",
+    },
+  ]
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+})
