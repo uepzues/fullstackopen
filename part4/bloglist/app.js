9bff57a173360037cc9f8b575b7ee266c21a892a
zuesuep <kimsanz@gmail.com>
2024-11-03 14:16:20 +0800

    imported middleware and logger

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,10 @@
 const { MONGODB_URI } = require("./utils/config")
+const middleware = require("./utils/middleware")
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const blogRouter = require("./controllers/blog")
+const logger = require("./utils/logger")
 
 const app = express()
 
@@ -11,13 +13,16 @@ const mongoUrl = MONGODB_URI
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    console.log("connected to DB")
+    logger.info("connected to DB")
   })
-  .catch((err) => console.log("error connecting to DB", err.message))
+  .catch((err) => logger.error("error connecting to DB", err.message))
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static("dist"))
 app.use("/api/blogs", blogRouter)
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
