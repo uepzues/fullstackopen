9fc3456a1fc4b69580afdcb6f290d494f0cecc56
zuesuep <kimsanz@gmail.com>
2024-11-03 14:16:34 +0800

    modified

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,7 @@
 const app = require("./app")
 const { PORT } = require("./utils/config")
+const logger = require("./utils/logger")
 
 app.listen(PORT, () => {
-  console.log("server running on port", PORT)
+  logger.info("server running on port", PORT)
 })
