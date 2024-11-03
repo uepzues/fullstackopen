918a338926d6fab125b6d8b6584939cc6e3e80c6
zuesuep <kimsanz@gmail.com>
2024-11-03 19:30:15 +0800

    refactored average

--- part4/notes-backend/utils/for_testing.js
+++ part4/notes-backend/utils/for_testing.js
@@ -1,5 +1,5 @@
 const reverse = (string) => {
-  return string.split("").reverse().join()
+  return string.split("").reverse().join("")
 }
 
 const average = (array) => {
@@ -7,7 +7,7 @@ const average = (array) => {
     return sum + item
   }
 
-  return array.reduce(reducer, 0) / array.length
+  return array.length === 0 ? 0:array.reduce(reducer, 0) / array.length
 }
 
 module.exports = {
