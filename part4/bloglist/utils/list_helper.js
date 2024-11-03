5ca4c4b078bd9684afdbbee259b227b469231c24
zuesuep <kimsanz@gmail.com>
2024-11-03 20:41:35 +0800

    4.4 added totalLikes function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blog) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  console.log(blogs.length)
+  if (!blogs || blogs.length === 0) {
+    return 0
+  }
+
+  if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    return blogs.reduce((sum, currBlog) => {
+      console.log(currBlog.likes)
+      return sum + currBlog.likes
+    }, 0)
+  }
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
