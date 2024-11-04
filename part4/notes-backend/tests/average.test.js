16e5a218450ad49f2ba56e1ac69776994874f013
zuesuep <kimsanz@gmail.com>
2024-11-04 13:59:49 +0800

    linted

--- part4/notes-backend/tests/average.test.js
+++ part4/notes-backend/tests/average.test.js
@@ -3,16 +3,16 @@ const assert = require("node:assert")
 
 const { average } = require("../utils/for_testing")
 
-describe("average", ()=> {
-    test("of one value is the value itself", ()=>{
-        assert.strictEqual(average([1]), 1)
-    })
+describe("average", () => {
+  test("of one value is the value itself", () => {
+    assert.strictEqual(average([1]), 1)
+  })
 
-    test('of many is calculated right', ()=>{
-        assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
-    })
+  test("of many is calculated right", () => {
+    assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
+  })
 
-    test("of empty array is zero", ()=> {
-        assert.strictEqual(average([]), 0)
-    })
+  test("of empty array is zero", () => {
+    assert.strictEqual(average([]), 0)
+  })
 })
