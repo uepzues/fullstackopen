ad9e8cecdd3c32e3f87a3c348561d35458b5b5d0
zuesuep <kimsanz@gmail.com>
2024-11-04 09:06:09 +0800

    added mostBlogs function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,7 @@
+const _ = require("lodash")
 const dummy = (blog) => {
   return 1
 }
-
 const totalLikes = (blogs) => {
   console.log(blogs.length)
   if (!blogs || blogs.length === 0) {
@@ -17,8 +17,25 @@ const totalLikes = (blogs) => {
     }, 0)
   }
 }
+const favouritesBlog = (blogs) => {
+  const likesMax = Math.max(...blogs.map((blog) => blog.likes))
+  console.log("likesMax", likesMax)
+  const maxNum = blogs.find((blog) => blog.likes === likesMax)
+  console.log("maxNum", maxNum.likes)
+  return maxNum.likes
+}
+
+const mostBlogs = (blog) => {
+  const getAuthor = _.reduce(blog, function (val, n) {
+    return val > n.likes ? val : n
+  })
+  console.log(getAuthor)
+  return { author: getAuthor.author, likes: getAuthor.likes }
+}
 
 module.exports = {
   dummy,
   totalLikes,
+  favouritesBlog,
+  mostBlogs,
 }
