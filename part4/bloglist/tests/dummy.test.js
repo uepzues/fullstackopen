5fb471854e1761bf601743cf0f6c16de6706169e
zuesuep <kimsanz@gmail.com>
2024-11-04 09:05:40 +0800

    4.6 added most blogs with author

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -27,3 +27,51 @@ describe("total likes", () => {
     assert.strictEqual(result, 5)
   })
 })
+
+describe("most likes", () => {
+  const blogList = [
+    {
+      title: "The Joy of Japanese KitKats: Unique Flavors You Need to Try",
+      author: "Aki Tanaka",
+      url: "http://localhost:3004/post",
+      likes: 5,
+      id: "6726e6f460db2ccaa9e7a2e0",
+    },
+    {
+      title: "Matcha Lovers Unite: Rich Matcha KitKats",
+      author: "Riku Tanabe",
+      url: "http://localhost:3004/post",
+      likes: 13,
+      id: "67271c2c1222e4c56ae1502d",
+    },
+  ]
+
+  test("highest number of likes", () => {
+    const result = listHelper.favouritesBlog(blogList)
+    assert.deepStrictEqual(result, 13)
+  })
+})
+
+describe("most number of likes with author name", () => {
+  const blogList = [
+    {
+      title: "The Joy of Japanese KitKats: Unique Flavors You Need to Try",
+      author: "Aki Tanaka",
+      url: "http://localhost:3004/post",
+      likes: 5,
+      id: "6726e6f460db2ccaa9e7a2e0",
+    },
+    {
+      title: "Matcha Lovers Unite: Rich Matcha KitKats",
+      author: "Riku Tanabe",
+      url: "http://localhost:3004/post",
+      likes: 13,
+      id: "67271c2c1222e4c56ae1502d",
+    },
+  ]
+
+  test("most number of likes with the author", () => {
+    const result = listHelper.mostBlogs(blogList)
+    assert.deepStrictEqual(result, { author: "Riku Tanabe", likes: 13 })
+  })
+})
