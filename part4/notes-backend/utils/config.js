5528046977116db51ef3ad3f23974f9990f39790
zuesuep <kimsanz@gmail.com>
2024-11-04 20:13:34 +0800

    modified

--- part4/notes-backend/utils/config.js
+++ part4/notes-backend/utils/config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const PORT = process.env.PORT || 3003
 const MONGODB_URI =
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === "development"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 
