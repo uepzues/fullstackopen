b12a445aa96106799f3e48a65f9d233debf52296
zuesuep <kimsanz@gmail.com>
2024-11-07 06:37:18 +0800

    added uri condition for testing db

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
 
 const PORT = process.env.PORT || 3004
-const MONGODB_URI = process.env.NODE_ENV
+const MONGODB_URI = process.env.NODE_ENV === "development"
+  ? process.env.DEV_MONGODB_URI : process.env.MONGODB_URI
 
 module.exports = {
   PORT,
