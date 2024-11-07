8e76033f2800c9827dbceccb6f68020b23c30d28
zuesuep <kimsanz@gmail.com>
2024-11-07 09:43:26 +0800

    recreated mongo.js

--- part4/notes-backend/mongo.js
+++ part4/notes-backend/mongo.js
@@ -1,1 +1,33 @@
+const mongoose = require("mongoose")
+// require("dotenv").config()
 
+const url = process.env.TEST_MONGODB_URI
+
+mongoose.set("strictQuery", false)
+
+mongoose
+  .connect(url)
+  .then(() => {
+    const noteSchema = new mongoose.Schema({
+      content: String,
+      important: Boolean,
+    })
+    console.log("connected to MongoDB")
+
+    const Note = mongoose.model("Note", noteSchema)
+    // noteSchema.set("toJSON", {
+    //   transform: (document, ret) => {
+    //     ret.id = ret._id.toString()
+    //     delete ret._id
+    //     delete ret.__v
+    //   },
+    // })
+
+    Note.find({}).then((res) => {
+      res.forEach((note) => {
+        console.log(note)
+      })
+      mongoose.connection.close()
+    })
+  })
+  .catch((err) => console.log("error connecting to DB", err))
