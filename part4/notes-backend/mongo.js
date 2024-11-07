8425740ac2c34279e57507cdf35ff023f8a07e4e
zuesuep <kimsanz@gmail.com>
2024-11-07 14:22:22 +0800

    commit

--- part4/notes-backend/mongo.js
+++ part4/notes-backend/mongo.js
@@ -5,29 +5,28 @@ const url = process.env.TEST_MONGODB_URI
 
 mongoose.set("strictQuery", false)
 
-mongoose
-  .connect(url)
-  .then(() => {
-    const noteSchema = new mongoose.Schema({
-      content: String,
-      important: Boolean,
-    })
-    console.log("connected to MongoDB")
-
-    const Note = mongoose.model("Note", noteSchema)
-    // noteSchema.set("toJSON", {
-    //   transform: (document, ret) => {
-    //     ret.id = ret._id.toString()
-    //     delete ret._id
-    //     delete ret.__v
-    //   },
-    // })
-
-    Note.find({}).then((res) => {
-      res.forEach((note) => {
-        console.log(note)
-      })
-      mongoose.connection.close()
-    })
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
   })
-  .catch((err) => console.log("error connecting to DB", err))
+  console.log("connected to MongoDB", "from mongo.js")
+
+  const Note = mongoose.model("Note", noteSchema)
+
+  // noteSchema.set("toJSON", {
+  //   transform: (document, ret) => {
+  //     ret.id = ret._id.toString()
+  //     delete ret._id
+  //     delete ret.__v
+  //   },
+  // })
+
+  Note.find({}).then((res) => {
+    res.forEach((note) => {
+      console.log("this is the note:", note)
+    })
+    mongoose.connection.close()
+  })
+})
+//   .catch((err) => console.log("error connecting to DB", err))
