21aed2f1db4463d3fe53282a3c174757f961baee
zuesuep <kimsanz@gmail.com>
2024-11-11 20:29:07 +0800

    removed clg for body

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -4,7 +4,7 @@ const requestLogger = (req, res) => {
   const logger = require("./logger")
   console.log("Method", req.method)
   console.log("Path", req.path)
-  console.log("Body", req.body)
+  // console.log("Body", req.body)
   console.log("---")
 }
 
