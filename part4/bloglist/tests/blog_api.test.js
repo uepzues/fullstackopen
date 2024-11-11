b426e5f307a6f358960d6bda07e6dc96c15df57f
zuesuep <kimsanz@gmail.com>
2024-11-11 20:32:37 +0800

    added beforeEach and after

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -7,3 +7,18 @@ const mongoose = require("mongoose")
 const api = supertest(app)
 const Blog = require("../models/blogModel")
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log("DB cleared")
+
+  for (const blog of blogHelper.blogList) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+
+
+after(async () => {
+  await mongoose.connection.close
+})
