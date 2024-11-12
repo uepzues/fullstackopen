485649a5c83c9dbeab650f9f9d29549be6e0f3d0
zuesuep <kimsanz@gmail.com>
2024-11-12 18:45:11 +0800

    expanded note schema

--- part4/notes-backend/models/model.js
+++ part4/notes-backend/models/model.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: { type: String, required: true, minlength: 5 },
   important: Boolean,
+  user: {
+    type: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
 })
 
 noteSchema.set("toJSON", {
