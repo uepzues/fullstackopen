9062c999a3b3fa605680f3a35fe135a40a4e3a0b
zuesuep <kimsanz@gmail.com>
2024-11-12 13:48:05 +0800

    4.14 added put router

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,5 +1,6 @@
 const blogRouter = require("express").Router()
 const Blog = require("../models/blogModel")
+const logger = require("../utils/logger")
 
 blogRouter.get("/", async (req, res) => {
   const result = await Blog.find({})
@@ -11,7 +12,7 @@ blogRouter.get("/:id", async (req, res) => {
   const result = await Blog.findById(id)
 
   if (result) {
-    res.json(result)
+    res.status(200).json(result)
   } else {
     res.status(404).send({ error: "blog not found" })
   }
@@ -24,16 +25,45 @@ blogRouter.post("/", async (req, res) => {
     title,
     author,
     url,
-    likes: likes || 0,
+    likes: likes,
   }
 
   const blog = new Blog(content)
-  if (!title || !url) {
-    return res.status(400).send({ error: "Title and URL required" })
+  if (!title) {
+    return res.status(400).send({ error: "Title required" })
+  }
+
+  if (!url) {
+    return res.status(400).send({ error: "URL required" })
   }
 
   const result = blog.save()
   return res.status(201).json(result)
+})
+
+blogRouter.put("/:id", async (req, res) => {
+  const id = req.params.id
+  const { title, author, url, likes } = req.body
+  const blogPost = {
+    title,
+    author,
+    url,
+    likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blogPost, {
+    new: true,
+    runValidators: true,
+    overwrite: true,
+    context: "query",
+  })
+  // logger.info("inside put", updatedBlog)
+
+  if (!updatedBlog) {
+    return res.status(400).send({ error: "blog not found" })
+  }
+
+  return res.status(200).json(updatedBlog)
 })
 
 blogRouter.delete("/:id", async (req, res) => {
