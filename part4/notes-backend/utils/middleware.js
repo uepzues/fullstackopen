7294ff020ba05a13c71d8f74feeead68542f26dc
zuesuep <kimsanz@gmail.com>
2024-11-22 11:55:58 +0800

    modified

--- part4/notes-backend/utils/middleware.js
+++ part4/notes-backend/utils/middleware.js
@@ -1,4 +1,4 @@
-const logger = require("./logger")
+// const logger = require("./logger")
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method)
   console.log("Path:  ", request.path)
@@ -8,7 +8,17 @@ const requestLogger = (request, response, next) => {
 }
 
 const errorHandler = (err, req, res, next) => {
-  logger.error(err.message)
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" })
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message })
+  } else if (
+    err.name === "MongoServerError" &&
+    err.message.includes("E11000 duplicate key error")
+  ) {
+    return res.status(400).json({ error: "expected 'username' to be unique" })
+  }
+  next(err)
 }
 
 const unknownEndpoint = (request, response) => {
