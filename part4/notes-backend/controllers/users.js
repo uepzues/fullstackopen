0f6c0c02924602dfbc084d694fa8236bb581e61e
zuesuep <kimsanz@gmail.com>
2024-11-24 16:37:37 +0800

    added userId

--- part4/notes-backend/controllers/users.js
+++ part4/notes-backend/controllers/users.js
@@ -2,20 +2,29 @@ const bcrypt = require("bcrypt")
 const userRouter = require("express").Router()
 const User = require("../models/user")
 
+userRouter.get("/", async (req, res) => {
+  const users = await User.find({})
+
+  res.json(users)
+})
+
 userRouter.post("/", async (req, res) => {
-  const { username, name, password } = req.body
+  const { username, name, password, id } = req.body
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
+  const user = await User.findById(id)
+
+  const notes = new User({
     username,
     name,
     passwordHash,
   })
 
-  const savedUser = await user.save()
-
-  res.status(201).json(savedUser)
+  const savedNote = await notes.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  res.status(201).json(savedNote)
 })
 
 module.exports = userRouter
