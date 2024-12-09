e9084db911c6b136603efff964d70f9abfcb7970
zuesuep <kimsanz@gmail.com>
2024-12-09 10:29:40 +0800

    added toen expire errror

--- part4/notes-backend/utils/middleware.js
+++ part4/notes-backend/utils/middleware.js
@@ -19,6 +19,8 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).json({ error: "expected 'username' to be unique" })
   } else if (err.name === "JsonWebTokenError") {
     return res.status(401).json({ error: "token invalid" })
+  } else if (err.name === "TokenExpiredError") {
+    return res.status(401).json({ error: "token expired" })
   }
   next(err)
 }
