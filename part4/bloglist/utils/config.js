08498e4ef36e4b317060828f41225ec1c1088512
zuesuep <kimsanz@gmail.com>
2025-01-04 07:15:13 +0800

    edited NODE_ENV

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,8 +1,10 @@
 require("dotenv").config()
 
 const PORT = process.env.PORT || 3004
-const MONGODB_URI = process.env.NODE_ENV === "test"
-  ? process.env.DEV_MONGODB_URI : process.env.MONGODB_URI
+const MONGODB_URI =
+  process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
+    ? process.env.DEV_MONGODB_URI
+    : process.env.MONGODB_URI
 
 module.exports = {
   PORT,
