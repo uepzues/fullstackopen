9ca4b2a25112dd54271001084ef707b9d9f5e3f7
zuesuep <kimsanz@gmail.com>
2025-01-22 15:09:19 +0800

    5.7 added buttonlabel

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,32 +1,34 @@
 import { useState, forwardRef, useImperativeHandle } from "react"
 
-const Togglable = forwardRef(({ children, buttonLabel }, refs) => {
-  const [visible, setVisible] = useState(false)
+const Togglable = forwardRef(
+  ({ children, buttonLabel1, buttonLabel2 }, refs) => {
+    const [visible, setVisible] = useState(false)
 
-  const hideWhenVisible = { display: visible ? "none" : "" }
-  const showWhenVisible = { display: visible ? "" : "none" }
+    const hideWhenVisible = { display: visible ? "none" : "" }
+    const showWhenVisible = { display: visible ? "" : "none" }
 
-  const toggleVisibility = () => setVisible(!visible)
+    const toggleVisibility = () => setVisible(!visible)
 
-  useImperativeHandle(refs, () => {
-    return { toggleVisibility }
-  })
+    useImperativeHandle(refs, () => {
+      return { toggleVisibility }
+    })
 
-  return (
-    <>
-      <div>
-        <div style={hideWhenVisible}>
-          <button onClick={toggleVisibility}>{buttonLabel}</button>
+    return (
+      <>
+        <div>
+          <div style={hideWhenVisible}>
+            <button onClick={toggleVisibility}>{buttonLabel1}</button>
+          </div>
+
+          <div style={showWhenVisible}>
+            {children}
+            <button onClick={toggleVisibility}>{buttonLabel2}</button>
+          </div>
         </div>
-
-        <div style={showWhenVisible}>
-          {children}
-          <button onClick={toggleVisibility}>Cancel</button>
-        </div>
-      </div>
-    </>
-  )
-})
+      </>
+    )
+  }
+)
 
 Togglable.displayName = "Togglable"
 
