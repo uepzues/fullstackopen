4b258686593910ad3b546aa479ebb9344ff29aeb
zuesuep <kimsanz@gmail.com>
2025-01-29 13:45:22 +0800

    added plugin

--- part5/bloglist-frontend/eslint.config.js
+++ part5/bloglist-frontend/eslint.config.js
@@ -17,12 +17,25 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: 'detect' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    env: {
+      'vitest-globals/env': true,
+      'vitest-globals': true,
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      'plugin:react-refresh/recommended',
+    ],
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
