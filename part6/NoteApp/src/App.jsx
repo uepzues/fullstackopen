af900c5638eef27779c2195d8f987d2a6124b5c4
uepzues <kimsanz@gmail.com>
2025-02-10 17:38:53 +0800

    noteApp commit / removed counter folder

---
+++ part6/NoteApp/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import noteReducer from './reducers/noteReducer'
+
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ part6/NoteApp/src/reducers/noteReducer.js
@@ -0,0 +1,56 @@
+import { legacy_createStore as createStore } from 'redux'
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.payload)
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const noteToChange = state.find((n) => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      }
+      return state.map((note) => (note.id !== id ? note : changedNote))
+    }
+    default:
+      return
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default noteReducer
