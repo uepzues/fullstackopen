7a28295547e573672fb9ecb9196913834f253bff
zuesuep <kimsanz@gmail.com>
2026-04-11 16:08:23 +0800

    Prt9 3.4-3.5

--- part9/fs-typescript/healthapp/bmi/bmiCalculator.ts
+++ part9/fs-typescript/healthapp/bmi/bmiCalculator.ts
@@ -1,15 +1,64 @@
-const calculateBmi = (height: number, weight: number): string =>{
-    const bmi = weight / (height/100)**2;
-    switch(true){
-        case bmi < 18.5:
-            return 'Underweight range';
-        case bmi >= 18.5 && bmi < 25:
-            return 'Normal range';
-        case bmi >= 25 && bmi < 30:
-            return 'Overweight range';
-        default:
-            return 'Obese range';
-    }
+interface Result {
+  weight: number;
+  height: number;
+  bmi: string;
+}
+const calculateBmi = (height: string, weight: string): Result => {
+  if (!height || !weight) {
+    console.log('please enter height and weight')
+    throw new Error('malformatted parameters');
+  }
+
+  const h = Number(height);
+  const w = Number(weight);
+
+  if (isNaN(h) || isNaN(w)) {
+    console.error('make sure height and weight are numbers');
+    throw new Error('malformatted parameters');
+  }
+
+  const bmi = w / (h / 100) ** 2;
+
+  let rating: string;
+
+  switch (true) {
+    case bmi < 18.5:
+      rating = 'Underweight range';
+      break;
+    case bmi >= 18.5 && bmi < 25:
+      rating = 'Normal range';
+      break;
+    case bmi >= 25 && bmi < 30:
+      rating = 'Overweight range';
+      break;
+    default:
+      rating = 'Obese range';
+  }
+  return {
+    weight: w,
+    height: h,
+    bmi: rating,
+  };
+};
+
+// console.log(calculateBmi(180, 74))
+
+const isMainModule = import.meta.filename === process.argv[1];
+// console.log('meta', import.meta.filename)
+// console.log('argv', process.argv[1])
+if (isMainModule) {
+  const [, , height, weight] = process.argv;
+
+  if (!height || !weight) {
+    console.error('Please input height and weight.');
+    process.exit(1);
+  }
+  try {
+    console.log(calculateBmi(height, weight));
+  } catch (error) {
+    console.error((error as Error).message);
+    process.exit(1);
+  }
 }
 
-console.log(calculateBmi(180, 74))
+export default calculateBmi;
--- part9/fs-typescript/healthapp/index.ts
+++ part9/fs-typescript/healthapp/index.ts
@@ -1,19 +1,32 @@
-import express from 'express'
+import express from 'express';
+import calculateBmi from './bmi/bmiCalculator.js';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (_req, res)=>{
-    res.send('Hello')
-})
+app.get('/', (_req, res) => {
+  res.send('Hello');
+});
 
-app.get('/hello', (_req, res)=>{
-    res.send('Hello Full Stack!')
-})
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
 
-const PORT = 3000
+app.get('/bmi', (req, res) => {
+  try {
+    const height = req.query.height as string;
+    const weight = req.query.weight as string;
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+    const result = calculateBmi(height, weight);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ error: (error as Error).message });
+  }
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
