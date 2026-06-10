bb6fbe7db65c073346be53ba56a25e92944edaa4
zuesuep <kimsanz@gmail.com>
2026-06-10 15:49:49 +0800

    formatted file

--- part9/fs-typescript/patientor/frontend/src/services/patients.ts
+++ part9/fs-typescript/patientor/frontend/src/services/patients.ts
@@ -1,28 +1,21 @@
-import axios from "axios";
-import { Entry, EntryWithoutId, Patient, PatientFormValues } from "../types";
+import axios from 'axios';
+import { Entry, EntryWithoutId, Patient, PatientFormValues } from '../types';
 
-import { apiBaseUrl } from "../constants";
+import { apiBaseUrl } from '../constants';
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(
-    `${apiBaseUrl}/patients`
-  );
+  const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`);
 
   return data;
 };
 
 const getPatient = async (id: string) => {
-  const { data } = await axios.get<Patient>(
-    `${apiBaseUrl}/patients/${id}`
-  )
-  return data
-}
+  const { data } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+  return data;
+};
 
 const create = async (object: PatientFormValues) => {
-  const { data } = await axios.post<Patient>(
-    `${apiBaseUrl}/patients`,
-    object
-  );
+  const { data } = await axios.post<Patient>(`${apiBaseUrl}/patients`, object);
 
   return data;
 };
@@ -30,12 +23,14 @@ const create = async (object: PatientFormValues) => {
 const addEntry = async (patientId: string, object: EntryWithoutId) => {
   const { data } = await axios.post<Entry>(
     `${apiBaseUrl}/patients/${patientId}/entries`,
-    object
+    object,
   );
   return data;
 };
 
 export default {
-  getAll, create, getPatient, addEntry
+  getAll,
+  create,
+  getPatient,
+  addEntry,
 };
-
